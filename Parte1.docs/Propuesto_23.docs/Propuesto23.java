f1b91af9ad5aa393b6026798a6ce08650fdc9664
Lauv27 <143473984+Lauv27@users.noreply.github.com>
2023-09-19 18:19:59 -0500

    Create Propuesto23.java

---
+++ Parte1.docs/Propuesto_23.docs/Propuesto23.java
@@ -0,0 +1,9 @@
+package propuesto23;
+
+public class Propuesto23 {
+    public static void main(String[] args) {
+        InterfazVentana ventana = new InterfazVentana();
+        ventana.setVisible(true);
+    }
+}
+
