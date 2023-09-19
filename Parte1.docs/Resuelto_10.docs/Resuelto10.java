f32f541a0133fcbf78007246d30be78a248dcf50
Lauv27 <143473984+Lauv27@users.noreply.github.com>
2023-09-19 18:12:15 -0500

    Create Resuelto10.java

---
+++ Parte1.docs/Resuelto_10.docs/Resuelto10.java
@@ -0,0 +1,8 @@
+package resuelto10;
+
+public class Resuelto10 {
+    public static void main(String[] args) {
+        InterfazVentana ventana = new InterfazVentana();
+        ventana.setVisible(true);
+    }
+}
