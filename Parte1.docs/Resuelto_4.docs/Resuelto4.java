3aa44046ea7c9a3b26c166818b1cc3e94e00dbeb
Lauv27 <143473984+Lauv27@users.noreply.github.com>
2023-09-19 18:09:15 -0500

    Create Resuelto4.java

---
+++ Parte1.docs/Resuelto_4.docs/Resuelto4.java
@@ -0,0 +1,8 @@
+package resuelto7;
+
+public class Resuelto7 {
+    public static void main(String[] args) {
+        InterfazVentana ventana = new InterfazVentana();
+        ventana.setVisible(true);
+    }
+}
