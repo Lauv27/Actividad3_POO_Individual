c27b0be1708d098b28aa4460ba38cab22c7cb571
Lauv27 <143473984+Lauv27@users.noreply.github.com>
2023-09-19 18:22:58 -0500

    Create Figuras_Geometricas.java

---
+++ Parte2.docs/Figuras_Geometricas.java
@@ -0,0 +1,8 @@
+package F_geometricas;
+
+public class F_geometricas {
+    public static void main(String[] args) {
+        InterfazPrincipal ventana = new InterfazPrincipal();
+        ventana.setVisible(true);
+    }
+}
