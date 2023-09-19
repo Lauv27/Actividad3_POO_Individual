6fa4a7417159e127e76cd4c2b6059500be33d609
Lauv27 <143473984+Lauv27@users.noreply.github.com>
2023-09-19 17:42:42 -0500

    Create Propuesto18.java

---
+++ Parte1.docs/Propuesto_18.docs/Propuesto18.java
@@ -0,0 +1,8 @@
+package propuesto_18;
+
+public class Propuesto18 {
+    public static void main(String[] args) {
+        InterfazUsuario ventanaPrincipal = new InterfazUsuario();
+        ventanaPrincipal.setVisible(true);
+    }
+}
