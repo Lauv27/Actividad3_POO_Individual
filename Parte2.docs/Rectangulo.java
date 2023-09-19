42f156e40ac2dc3c5f0c539152a71ed5a79047f9
Lauv27 <143473984+Lauv27@users.noreply.github.com>
2023-09-19 18:30:44 -0500

    Create Rectangulo.java

---
+++ Parte2.docs/Rectangulo.java
@@ -0,0 +1,19 @@
+package F_geometricas;
+
+public class Rectangulo {
+    double base, altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        return base * altura;
+    }
+
+    public double calcularPerimetro() {
+        return (2 * base) + (2 * altura);
+    }
+}
+
