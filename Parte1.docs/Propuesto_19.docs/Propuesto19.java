3f33b29b46c771045e17944f98a96a571ca61833
Lauv27 <143473984+Lauv27@users.noreply.github.com>
2023-09-19 17:46:40 -0500

    Create Propuesto19.java

---
+++ Parte1.docs/Propuesto_19.docs/Propuesto19.java
@@ -0,0 +1,8 @@
+package propuesto19;
+
+public class Propuesto19 {
+    public static void main(String[] args) {
+        InterfazVentana ventana = new InterfazVentana();
+        ventana.setVisible(true);
+    }
+}
